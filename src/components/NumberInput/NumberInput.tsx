16fb3aeeff60e04c335d8f2b065fd82e4da32694
johannes <joe.wipper@gmx.de>
2023-02-02 19:24:58 +0100

    [NumberInput] add stepping (+/- buttons)

--- src/components/NumberInput/NumberInput.tsx
+++ src/components/NumberInput/NumberInput.tsx
@@ -1,4 +1,4 @@
-import {FocusEvent, HTMLAttributes, KeyboardEvent, useRef} from 'react'
+import {FocusEvent, HTMLAttributes, KeyboardEvent, useRef, useState} from 'react'
 import RoundDiv from 'react-round-div'
 import {evaluate, Unit} from 'mathjs'
 import convert from 'convert'
@@ -7,7 +7,7 @@ import styles from './number-input.module.scss'
 import {cl} from '../../utils/class-names'
 
 type NumberInputPropsType = {
-    defaultValue?: number,
+    defaultValue?: number | string,
     step?: number
 } & HTMLAttributes<HTMLElement>
 
@@ -15,9 +15,12 @@ const defaultUnit = 'cm'
 
 export default function NumberInput({defaultValue = 0, step = .1, ...props}: NumberInputPropsType) {
     const innerWrapper = useRef(null)
+    const input = useRef(null)
+    const [valueBeforeFocus, setValueBeforeFocus] = useState(convertInputValue(defaultValue.toString()))
 
     function focus(e: FocusEvent<HTMLInputElement>) {
-        (innerWrapper.current as HTMLElement | null)?.parentElement?.classList.add(styles.focus)
+        setValueBeforeFocus((e.target as HTMLInputElement).value)
+        ;(innerWrapper.current as HTMLElement | null)?.parentElement?.classList.add(styles.focus)
         e.target.addEventListener('mouseup', () => {
             (e.target as HTMLInputElement).setSelectionRange(0, (e.target as HTMLInputElement).value.length)
         }, {once: true})
@@ -38,25 +41,36 @@ export default function NumberInput({defaultValue = 0, step = .1, ...props}: Num
     }
 
     function convertInputValue(value: string): string {
-        const result = evaluate(value)
-        if (result.constructor.name === 'Unit') {
-            const resultJson = result.toJSON()
-            if (resultJson.value === 0)
-                return '0' + defaultUnit
-            const conversion = convert(resultJson.value, resultJson.unit).to('best')
-            conversion.quantity = roundToPlace(conversion.quantity as number, 6)
-            return conversion.quantity + conversion.unit
-        } else {
-            return roundToPlace(Number(value), 6) + defaultUnit
+        try {
+            const result = evaluate(value)
+            if (result.constructor.name === 'Unit') {
+                const resultJson = result.toJSON()
+                if (resultJson.value === 0)
+                    return '0' + defaultUnit
+                const conversion = convert(resultJson.value, resultJson.unit).to('best')
+                conversion.quantity = roundToPlace(conversion.quantity as number, 6)
+                return conversion.quantity + conversion.unit
+            } else {
+                return roundToPlace(Number(value), 6) + defaultUnit
+            }
+        } catch (err) {
+            return valueBeforeFocus
+        }
+    }
+
+    function offsetValue(step: number, unit: string = defaultUnit) {
+        const newValue = convertInputValue((input.current as HTMLInputElement | null)?.value + `+(${step}${unit})`)
+        if (input.current) {
+            (input.current as HTMLInputElement).value = newValue
         }
     }
 
     return <RoundDiv {...props} className={cl(props.className, styles.numberInput)}>
         <div className={styles.innerWrapper} ref={innerWrapper}>
-            <button className={cl(styles.button, styles.subtract)}>-</button>
-            <input className={cl(styles.input)} type={'text'} defaultValue={convertInputValue(defaultValue.toString())}
-                   onFocus={focus} onBlur={blur} onKeyDown={blurOnEnter}/>
-            <button className={cl(styles.button, styles.add)}>+</button>
+            <button className={cl(styles.button, styles.subtract)} onClick={() => offsetValue(-step)}>-</button>
+            <input className={cl(styles.input)} type={'text'} defaultValue={valueBeforeFocus}
+                   onFocus={focus} onBlur={blur} onKeyDown={blurOnEnter} ref={input}/>
+            <button className={cl(styles.button, styles.add)} onClick={() => offsetValue(step)}>+</button>
         </div>
     </RoundDiv>
 }
